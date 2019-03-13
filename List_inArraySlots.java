158e8b44526c7cde29735a514b1b551e48ee46fe
Tanvir Ahmed <tahmed5@stuy.edu>
2019-03-13 02:56:54 +0000

    Need Help...

--- List_inArraySlots.java
+++ List_inArraySlots.java
@@ -49,7 +49,7 @@ public class List_inArraySlots {
      public String toString() {
        String returnString = "[";
        for(int index = 0; index < filledElements; index ++){
-         if(typeOfElements[index] == 0)returnString = returnString + intElements[index] + ",";
+         if(typeOfElements[index] == 0) returnString = returnString + intElements[index] + ",";
          if(typeOfElements[index] == 1) returnString = returnString + doubleElements[index] + ",";
          else returnString = returnString + stringElements[index] + ",";
        }
@@ -63,25 +63,85 @@ public class List_inArraySlots {
 
       @return true, in keeping with conventions yet to be discussed
      */
-     // public boolean add( int type   // same meaning as in typeOfElements
-                       // , int    intValue
-                       // , double doubleValue
-                       // , String stringValue
-                       // ) {
-     // }
+     public boolean add( int type   // same meaning as in typeOfElements
+                       , int    intValue
+                       , double doubleValue
+                       , String stringValue
+                       ) {
+        filledElements++;
+        if(filledElements == typeOfElements.length){
+            expand();
+        }
+        
+        else if (type == 0) {
+            typeOfElements[filledElements - 1] = 0;
+            intElements[filledElements - 1] = intValue;
+        }
+        
+        else if (type == 1) {
+            typeOfElements[filledElements - 1] = 1;
+            doubleElements[filledElements - 1] = doubleValue;
+        }
+        
+        else if (type == 2) {
+            typeOfElements[filledElements - 1] = 2;
+            stringElements[filledElements - 1] = stringValue;
+        }
+        
+        return true;
+     }
 
 
     /**
       Double the capacity of the List_inArraySlots,
       preserving existing data.
      */
-     // private void expand() {
-        // System.out.println( "expand... (for debugging)");
-           // /* S.O.P. rules for debugging:
-              // Working methods should be silent. But during
-              // development, the programmer must verify that
-              // this method is called when that is appropriate.
-              // So test using the println(), then comment it out.
-              // */
-     // }
+     private void expand() {
+        //expanding typeOfElements
+        int[] extraHolder = new int[filledElements];
+        for(int index = 0; index < filledElements; index ++){
+            extraHolder[index] = typeOfElements[index];
+          }
+        typeOfElements = new int[filledElements + 10];
+        for(int index = 0; index < filledElements; index ++){
+          typeOfElements[index] = extraHolder[index];
+        }
+        
+        //expanding intElements
+        int[] intExtraHolder = new int[filledElements];
+        for(int index = 0; index < filledElements; index ++){
+            intExtraHolder[index] = intElements[index];
+          }
+        typeOfElements = new int[filledElements + 10];
+        for(int index = 0; index < filledElements; index ++){
+          intElements[index] = intExtraHolder[index];
+        }
+        
+        //expanding doubleElements
+        double[] doubleExtraHolder = new double[filledElements];
+        for(int index = 0; index < filledElements; index ++){
+            doubleExtraHolder[index] = doubleElements[index];
+          }
+        typeOfElements = new int[filledElements + 10];
+        for(int index = 0; index < filledElements; index ++){
+          doubleElements[index] = doubleExtraHolder[index];
+        }
+        
+        //expanding stringElements
+        String[] stringExtraHolder = new String[filledElements];
+        for(int index = 0; index < filledElements; index ++){
+            stringExtraHolder[index] = stringElements[index];
+          }
+        typeOfElements = new int[filledElements + 10];
+        for(int index = 0; index < filledElements; index ++){
+          stringElements[index] = stringExtraHolder[index];
+        }
+        System.out.println( "expand... (for debugging)");
+           /* S.O.P. rules for debugging:
+              Working methods should be silent. But during
+              development, the programmer must verify that
+              this method is called when that is appropriate.
+              So test using the println(), then comment it out.
+              */
+     }
 }
